6cf557ada5ac5c281bf8501f00cc1e5fc85debdb
Angela <afarnell1972@gmail.com>
2022-03-28 18:40:15 -0500

    Random Generator code

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Start your code ***HERE*** =========
 
-// create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+
+ //create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+let chars = ['!', '"', '#', '$', '%', '&', '', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '_', '`', 'a', 'b', 'c','d', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
 
 // create a global variable called "pwLength" with a number between 10 and 18
+let pwLength = 14;
+
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
-
+function addNewPassword (){
+  let password="";
+    for( let i=0; i<=pwLength; i++){
+      let randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars[randomNumber];
+  }
+  return password
+}
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
 let genBtn = document.getElementById("btnGen");
